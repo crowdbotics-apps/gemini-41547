b476efe18d1a430dcb28a24c49b01d5460bf03c0
Crowdbotics <team@crowdbotics.com>
2023-06-16 21:00:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gemini-41547",
+  url: "https://gemini-41547.botics.co",
+  api: "https://gemini-41547.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
